240d32b86cd803e2d966f28b2efa11b11756c7ac
shenq <14640532+shenq123@user.noreply.gitee.com>
2025-03-06 17:43:57 +0800

    homework3

---
+++ assignment2/homework3/Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3
+{
+    class Calculator
+    {
+        static public string  GetNumber(int n)
+        {
+            
+           
+                
+            bool[] isPrime = new bool[n + 1];
+            for (int i = 2; i <= n; i++)
+            {
+                isPrime[i] = true;
+            }
+
+                
+            for (int i = 2; i * i <= n; i++)
+            {
+                if (isPrime[i])
+                {
+                        
+                    for (int j = i * i; j <= n; j += i)
+                    {
+                        isPrime[j] = false;
+                    }
+                }
+            }
+            string data="";
+            for (int i = 2; i <= n; i++)
+            {
+                if (isPrime[i])
+                {
+                    data += $"{i} ";
+                }
+            }
+            return data;
+        
+
+    }
+}
+}
---
+++ assignment2/homework3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Output.SendOutput(new string[]{ Calculator.GetNumber(100)},"结果为:");
+        }
+    }
+}
